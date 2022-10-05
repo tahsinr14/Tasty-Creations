ac90c8458a983ff0eace0f282c6ec8be6a79b92f
Taimoor Dawami <tdawami1@gmail.com>
2022-10-05 17:32:11 -0400

    Add configuration for client side routing

--- tasty-creations/src/App.js
+++ tasty-creations/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import LoginForm from './components/loginForm'
+import RegisterForm from './components/registerForm';
 
 function App() {
   return (
     <div className="page">
-      <LoginForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm/>}/>
+          <Route path="/login" element={<LoginForm/>}/>
+          <Route path="/register" element={ <RegisterForm/>}/> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
